390e0f21f3625fbe5c3472cdc24e0dd40fae9e49
Muhammad Sufyan <syedsufyan4585@gmail.com>
2025-04-12 12:49:20 +0500

    Add files via upload

---
+++ assignment-4 ( 00-05 )/assignment 4 ( 00-05 ).py
@@ -0,0 +1,55 @@
+# 01_add_two_numbers -------------------------------------------------------------
+
+# def main():
+
+#     first_num = int(input("Enter the first number: "))
+#     second_num = int(input("Enter the second number: "))
+
+#     sum = first_num + second_num
+
+#     print("The sum of", first_num, "and", second_num, "is", sum)
+
+# if __name__ == '__main__':
+#     main()
+
+# 02_agreement_bot ----------------------------------------------------------------
+
+# def main():
+
+#     user_fav: str = input("What is your favorite animal?: ")
+#     print()
+#     print("My favorite animal is also", user_fav)
+
+# if __name__ == '__main__':
+#     main()
+
+# 03_fahrenheit_to_celsius --------------------------------------------------------
+
+# def main():
+
+#     degrees_fahrenheit = int(input("Enter temperature in Fahreinheit: "))
+#     degrees_celsius = (degrees_fahrenheit - 32) * 5.0/9.0
+#     print("Temperature: ", degrees_fahrenheit,'F =',degrees_celsius,'C')
+
+# if __name__ == '__main__':
+#     main() 
+
+# 04_how_old_are_they --------------------------------------------------------
+
+# def main():
+#     anton: int = 21
+#     beth : int = 6 + anton
+#     chen : int = 20 + beth
+#     drew : int = chen + anton
+#     ethan : int = chen
+
+#     print('Anton age is: ', str(anton))
+#     print('Beth age is: ', str(beth))
+#     print('Chen age is: ', str(chen))
+#     print('Drew age is: ', str(drew))
+#     print('Ethan age is: ', str(ethan))
+
+# if __name__ == '__main__':
+#     main()
+
+# 05_riangle_perimeter --------------------------------------------------------
